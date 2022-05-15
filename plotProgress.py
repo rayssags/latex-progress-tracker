878bb9badb2dd4e494e045a5b7fb811ed0177cd1
Rayssa Guimarães Silva <40724753+rayssags@users.noreply.github.com>
2022-05-14 23:30:35 -0300

    Update plotProgress.py

--- plotProgress.py
+++ plotProgress.py
@@ -2,10 +2,13 @@ from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.dates import MO, WeekdayLocator
 import os
 import sys
 import numpy as np
 import seaborn as sns
+
+plt.style.use("rayssa")
 savedir = os.getcwd()+"/"
 output_format = ".png"
 
@@ -16,15 +19,15 @@ df = df.set_index('timestamp_fixed')
 current_pagecount = df["pagecount"].iloc[-1]
 current_wordcount = df["wordcount"].iloc[-1]
 
-wc_color="orangered"
-pc_color="royalblue"
+wc_color=plt.rcParams['axes.prop_cycle'].by_key()['color'][0]
+pc_color=plt.rcParams['axes.prop_cycle'].by_key()['color'][1]
 
 # Plot with Combined progress
 months = mdates.MonthLocator()  # every month
 days = mdates.DayLocator()  # every day
-date_fmt = mdates.DateFormatter('%b %Y')
+date_fmt = mdates.DateFormatter('%d %b %Y')
 
-fig, ax = plt.figure(), plt.gca()
+fig, ax = plt.figure(figsize=(8,7)), plt.gca()
 ax2= ax.twinx()
 df.plot(y='pagecount',legend=None, ax=ax, color=pc_color)
 df.plot(y='wordcount', legend=None, ax=ax2, color=wc_color)
@@ -32,27 +35,27 @@ df.plot(y='wordcount', legend=None, ax=ax2, color=wc_color)
 ax.set_ylabel("Page Count", fontsize=16, color=pc_color)
 ax2.set_ylabel("Word Count", fontsize=16,color=wc_color)
 ax.set_xlabel('Date',fontsize=16)
-ax.set_ylim(ymin=0)
-ax2.set_ylim(ymin=0)
+ax.set_ylim(ymin=42)
+ax2.set_ylim(ymin=42)
 
-plt.annotate("{:,d} Pages".format(current_pagecount), xy=(.05,.88), xycoords="axes fraction",color=pc_color, fontsize=14)
-plt.annotate("{:,d} Words".format(current_wordcount), xy=(.05,.8), xycoords="axes fraction",color=wc_color, fontsize=14)
+plt.annotate("{:0.0f} Pages".format(current_pagecount), xy=(.05,.88), xycoords="axes fraction",color=pc_color, fontsize=14)
+plt.annotate("{:0.0f} Words".format(current_wordcount), xy=(.05,.8), xycoords="axes fraction",color=wc_color, fontsize=14)
 
-#ax.xaxis.set_major_locator(months) Uncomment if you want month-level updates
-ax.xaxis.set_minor_locator(days)
+ax.xaxis.set_major_locator(months) #Uncomment if you want month-level updates
+ax.xaxis.set_minor_locator(WeekdayLocator(byweekday=MO, interval=1))
 ax.xaxis.set_major_formatter(date_fmt)
+ax.xaxis.set_minor_formatter(mdates.DateFormatter('%d'))
+ax.tick_params(axis='both', which='minor', labelsize=8)
+
 
 # Uncomment if you want vertical lines at important points
-"""
 dates = {}
-dates["Detector chapter \nsent for edits "] = "2019-09-22 15:30:00"
-dates["Theory chapter \nsent for edits "] = "2019-10-20 21:57:00"
+dates["Voltei a frequentar o OV\n"] = "2022-04-14 15:30:00"
 
 for title,dt in dates.items():
     xloc = pd.to_datetime(dt)
     ax.axvline(x=xloc, color='firebrick', linestyle='--',)
     plt.annotate(title, xy=(xloc, 100), color="firebrick", fontsize=10, ha="right")
-"""
 
 ax.tick_params(labelsize=12)
 ax2.tick_params(labelsize=12)
